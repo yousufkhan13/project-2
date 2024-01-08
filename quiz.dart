ac10cbaaad35990dffb6129e37472e07a6c4c68a
yousufkhan13 <khanzzyyy@gmail.com>
2024-01-08 19:55:33 +0500

    quiz

---
+++ quiz.dart
@@ -0,0 +1,13 @@
+void main() {
+  String username = "yousufkhan13";
+  String password = "saylani123";
+  if (username == "yousufkhan13" || password == "saylani123") {
+    if (username != "yousufkhan13") {
+      print("username is wrong");
+    } else if (password != "saylani123") {
+      print("password is wrong");
+    } else {
+      print("username and password both are wrong");
+    }
+  }
+}
