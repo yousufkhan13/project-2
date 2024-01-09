278db5fb842d27c39f41785aeaab3f022a8dc4b3
yousufkhan13 <khanzzyyy@gmail.com>
2024-01-09 17:06:22 +0500

    3rd edition

--- list_ch.dart
+++ list_ch.dart
@@ -9,4 +9,28 @@ void main() {
   print(dekho);
   print(check);
   print(surnames);
+  List indexes = [0, 1, 2, 3, 4, 5, 6];
+  if (indexes.isEmpty) {
+    print("indexes is empty");
+    indexes.add(7);
+    indexes.add(8);
+    indexes.add(9);
+  }
+  if (indexes.isNotEmpty) {
+    print("list is not empty");
+  }
+  indexes.clear();
+  if (indexes.isEmpty) {
+    print("list is empty");
+  }
+  List vals = [1, 2, 3];
+  vals.add(4);
+  vals.addAll([5, 6, 7]);
+  vals.insert(0, 0);
+  vals.insertAll(1, [8, 9, 10]);
+  print(vals);
+  vals.remove(1);
+  print(vals);
+  vals.removeAt(vals.length - 1);
+  print(vals);
 }
