0a7f3308e4d08b5cad9762a3c02d698ae3c818c4
yousufkhan13 <khanzzyyy@gmail.com>
2024-01-09 16:45:40 +0500

    2nd edition

--- list_ch.dart
+++ list_ch.dart
@@ -2,8 +2,11 @@ void main() {
   List<String> surnames = ["khan", "rehman", "tatrool", "chumancha"];
   var dekho = surnames.length;
   var check = surnames.elementAt(1);
-  var gangoo = surnames.replaceRange(0, 1, ["1", "2", "3", "4"]);
+  surnames.replaceRange(0, 1, ["1", "2", "3", "4"]);
   List names = ["java", "angular", "vue", "kotlin"];
   names.sort();
   print(names);
+  print(dekho);
+  print(check);
+  print(surnames);
 }
